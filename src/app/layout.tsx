b333a927105657948629031c2446b3d057dea65c
rohannrk <rohanrk39357@gmail.com>
2023-10-08 11:36:59 +0530

    Upload functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Providers from "@/components/Providers";
 
 import "react-loading-skeleton/dist/skeleton.css"
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
             inter.className
           )}
         >
+          <Toaster/>
           <Navbar />
           {children}
         </body>
